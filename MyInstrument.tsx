2ce82f5de05d11dc5d2e15c1a7157b1a9a2feef9
Neil Enns <{ID}+{username}@users.noreply.github.com>
2023-02-04 13:03:45 -0800

    Working with Simbrief API!

--- MyInstrument.tsx
+++ MyInstrument.tsx
@@ -1,6 +1,6 @@
 import { FSComponent, Subject } from 'msfssdk';
 import { MyComponent } from './MyComponent';
-import type Server from 'node-vatsim/Server';
+// import type Server from 'node-vatsim/Server';
 
 class MyInstrument extends BaseInstrument {
   override get templateID(): string {
@@ -14,12 +14,15 @@ class MyInstrument extends BaseInstrument {
     FSComponent.render(<MyComponent atis={atis} />, document.getElementById('InstrumentContent'));
 
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://status.vatsim.net/status.json", true);
-    xhr.responseType = "json";
-    xhr.onreadystatechange = function () {
+//    xhr.open("GET", "https://status.vatsim.net/status.json", true);
+//    xhr.open("GET", "https://datis.clowd.io/api/kpdx", true);
+      xhr.open("GET", "https://www.simbrief.com/api/xml.fetcher.php?userid=369503&json=1");
+      xhr.responseType = "json";
+      xhr.onreadystatechange = function () {
       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        const data = xhr.response as Server;
-        console.log(data);
+//        const data = xhr.response as Server;
+        atis.set(JSON.stringify(xhr.response));
+        console.log(xhr.response);
       }
     };
     xhr.send();
