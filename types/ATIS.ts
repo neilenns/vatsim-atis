81e1f9ffeb69ff89e99ecf8cc9a9f0e1d0d3d1e9
Neil Enns <{ID}+{username}@users.noreply.github.com>
2023-02-06 10:53:03 -0800

    Add doc comments

---
+++ types/ATIS.ts
@@ -0,0 +1,22 @@
+interface ATIS {
+  /**
+   * ICAO for the airport reporting the ATIS information.
+   */
+  icao: string;
+  /**
+   * Source for the ATIS. One of: faa, vatsim, ivao, pilotedge.
+   */
+  source: string;
+  /**
+   * Combined ATIS for both arrivals and departures.
+   */
+  combined?: string;
+  /**
+   * ATIS for arrivals.
+   */
+  arr?: string;
+  /**
+   * ATIS for departures.
+   */
+  dep?: string;
+}
